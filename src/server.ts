7e917f0bebded22b1b8c52fe857c08216461e372
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-07-20 20:32:51 -0300

    feat: config eslint

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ app.listen({
     host: '0.0.0.0',
     port: env.PORT
 }).then(() => {
-    console.log('HTTP Server is running')
+    console.log(`\x1b[32m HTTP Server is running on ${env.PORT} \x1b[0m`)
 })
