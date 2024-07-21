cb0a870a158ee7be9df7215debdfa18fdfc17fcd
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-07-21 03:15:32 -0300

    feat: prisma client connection

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import { z } from 'zod'
+import { prismaClient } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/user',  async (request, response) => {
+    const bodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string()
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+    await prismaClient.user.create({
+        data: { name, email, password_hash: password }
+    })
+
+    return response.status(201).send()
+})  
